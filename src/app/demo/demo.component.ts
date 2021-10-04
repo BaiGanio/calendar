a87bf5084b5a631c9112027b9ed84dc2313bae6f
Lyuben Kikov <exceptionhelper@gmail.com>
2021-10-05 00:16:52 +0300

    nothing special

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
+import { startOfDay } from 'date-fns';
 
 @Component({
   selector: 'app-demo',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
   viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  events: CalendarEvent[] = [
+    {
+      start: startOfDay(new Date()),
+      title: 'First event',
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'Second event',
+    }
+  ]
+
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    console.log(date);
+    //let x=this.adminService.dateFormat(date)
+    //this.openAppointmentList(x)
   }
 
 }
